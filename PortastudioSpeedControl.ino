10db22a98f4b68f504bd08bda3d1fb22a701455f
Chip Audette <chipaudette@yahoo.com>
2014-03-09 22:23:48 -0400

    At startup, sends default voltage, ignoring the knob.

--- PortastudioSpeedControl.ino
+++ PortastudioSpeedControl.ino
@@ -12,17 +12,32 @@
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 const int analogOutPin = 6; // Analog output pin that the LED is attached to
 
-int sensorValue = 0;        // value read from the pot
-int outputValue = 0;        // value output to the PWM (analog out)
-int initSensorValue = 0;
+
+#define STATE_STILL_INITIAL_OUTPUT (1)
+#define STATE_NORMAL (2)
+int operating_state = STATE_STILL_INITIAL_OUTPUT;
+
+
+float initSensorValue_V = 0;
+#define THRESHOLD_VOLTS (0.2f) //for determining if we've departed from the initial reading
+float prev_value = 0.0f;
+float learn_fac = 0.25f;
 
 float default_output_V = 1.67f;
 
 #define OUTPUT_RANGE_V (5.0f)
 #define OUTPUT_RANGE_COUNTS (256)
+#define INPUT_RANGE_COUNTS (1024)
+#define INPUT_RANGE_V (5.0f)
+
+float convertInputCountsToVolts(int input_counts) {
+  return map((float)input_counts,0.0f,(float)INPUT_RANGE_COUNTS,0.0f,INPUT_RANGE_V);
+}
+
 void writeOutputVoltage(int analogPin,float voltage) {
   float output_float = map(voltage,0.0f,OUTPUT_RANGE_V,0.0f,(float)OUTPUT_RANGE_COUNTS);
-  analogWrite(analogPin,(int)(output_float+0.5f));
+  int output_counts = (int)(output_float+0.5f);
+  analogWrite(analogPin,output_counts);
 }
   
 
@@ -37,34 +52,27 @@ void setup() {
   writeOutputVoltage(analogOutPin,default_output_V);
   
   //get the current potentiometer value
-  initSensorValue = analogRead(analogInPin);
+  initSensorValue_V = convertInputCountsToVolts(analogRead(analogInPin));
   delay(10);
 }
 
-float prev_value = 0.0f;
-float learn_fac = 0.25f;
+
 void loop() {
   // read the analog in value:
-  sensorValue = analogRead(analogInPin); 
-
-  //compute smoothed version
-  float value = (float)sensorValue;
-  value = (1.0-learn_fac)*prev_value + learn_fac*value;
-  prev_value = value;
-  sensorValue = (int)value;
+  float sensorValue_V = convertInputCountsToVolts(analogRead(analogInPin));
   
-  // map it to the range of the analog out:
-  outputValue = map(sensorValue, 0, 1023, 0, 255);  
-  // change the analog out value:
-  analogWrite(analogOutPin, outputValue);           
-
-  // print the results to the serial monitor:
-//  Serial.print("sensor = " );                       
-//  Serial.print(sensorValue);      
-//  Serial.print("\t output = ");      
-//  Serial.println(outputValue);   
-
-  delay(10);                     
+  switch (operating_state) {
+    case (STATE_STILL_INITIAL_OUTPUT):
+      if (abs(sensorValue_V - initSensorValue_V) > THRESHOLD_VOLTS) {
+        operating_state = STATE_NORMAL;  //switch to the normal mode of operation
+      }
+      break;
+    case (STATE_NORMAL):
+      writeOutputVoltage(analogOutPin,sensorValue_V);
+  }
+  
+  delay(10); //keep the sample rate at a reasonable speed;
 }
+
 
 
